c6b16c6d382dba0c2967da49b0fe8ae78e19b01d
Tiger <tiger@TigerdeMacBook-Pro.local>
2020-12-01 19:09:18 +0800

    fix: ignore react props children

--- packages/eslint-config/rules/react.js
+++ packages/eslint-config/rules/react.js
@@ -9,6 +9,7 @@ module.exports = {
     'react-hooks/exhaustive-deps': 0,
     'react/jsx-uses-vars': 2,
     'react-hooks/rules-of-hooks': 2,
+    'react/prop-types': [2, { ignore: ['children'] }],
   },
   settings: {
     react: {
